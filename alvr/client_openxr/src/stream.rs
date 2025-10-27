05187f8b28879a08c1d6d300d36e3ce439a1c432
shinyquagsire23 <mtinc2@gmail.com>
2025-10-27 14:52:56 -0600

    Android OpenXR changes

--- alvr/client_openxr/src/stream.rs
+++ alvr/client_openxr/src/stream.rs
@@ -198,8 +198,8 @@ impl StreamContext {
             ],
             format,
             config.foveated_encoding_config.clone(),
-            platform != Platform::Lynx && !((platform.is_pico()) && config.enable_hdr),
-            config.use_full_range && !config.enable_hdr, // TODO: figure out why HDR doesn't need the limited range hackfix in staging?
+            (platform != Platform::Lynx) && !((platform.is_pico() || (platform == Platform::AndroidUnknown)) && config.enable_hdr),
+            (platform != Platform::AndroidUnknown) && config.use_full_range && !config.enable_hdr, // TODO: figure out why HDR doesn't need the limited range hackfix in staging?
             config.encoding_gamma,
             config.upscaling.clone(),
         );
